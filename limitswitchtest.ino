269700f134d77f4b5370ab05ced9f9d0cdb772a8
Yin <62259841+Yincard@users.noreply.github.com>
2024-03-28 19:40:51 -0400

    Update limitswitchtest.ino

--- limitswitchtest.ino
+++ limitswitchtest.ino
@@ -2,15 +2,12 @@ const int switchPin1 = 1;
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(switchPin1, INPUT);
-  digitalWrite(switchPin1, HIGH);
-  
+  pinMode(switchPin1, INPUT);  
 }
 
 void loop() {
   int state1 = digitalRead(switchPin1);
   Serial.print("Limit Switch");
-  Serial.print(state1 == HIGH ? "Open" : "Closed");
+  Serial.print(state1 == HIGH ? "Activated" : "Idle");
   delay(100);
 }
