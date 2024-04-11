58cf298468e698cdbaab09a4496dedab607af75b
Yin <62259841+Yincard@users.noreply.github.com>
2024-04-11 18:20:07 -0400

    Update whackamole-dev.ino

--- whackamole-dev.ino
+++ whackamole-dev.ino
@@ -1,16 +1,16 @@
 #include <Servo.h>
 #include <ezButton.h>
 
-const int limitSwitchPin = 8;
-const int servoPin = 7;
+const int limitSwitchPin = 7;
+const int servoPin = 9;
 Servo servo;
 ezButton limitSwitch(limitSwitchPin);
 
 enum LimitSwitchState { UP,
                         DOWN };
-enum ServoPosition { MOLE_UP = 130,
-                     MOLE_DOWN = 0 };
-
+enum ServoPosition { MOLE_UP = 180,
+                     MOLE_DOWN = 10 };
+//25
 LimitSwitchState limitSwitchState = DOWN;
 
 bool startedPrinted = false;
@@ -18,21 +18,20 @@ bool hitScorePrinted = false;
 bool endedPrinted = false;
 
 constexpr unsigned long gameDuration = 60000;  
-constexpr int HIT_DELAY = 500;
+constexpr int HIT_DELAY = 1500;
 
 unsigned long previousMillis = 0;
 unsigned long hitTime = 0;
 unsigned long moleHitStartTime = 0;
 bool moleHitDelay = false;
-int moleStayTime = 1500;  // Start with a higher value
+int moleStayTime = 800;  // Start with a higher value
 
 unsigned long gameStartTime;
-int score = 0;
 
 void setup() {
   limitSwitch.setDebounceTime(50);
-  servo.attach(servoPin);
   servo.write(MOLE_DOWN);
+  servo.attach(servoPin);
 
   Serial.begin(9600);
   gameStartTime = millis();
@@ -42,7 +41,7 @@ void loop() {
   unsigned long currentMillis = millis();
   if ((currentMillis - gameStartTime) <= gameDuration) {  // Ensure game conditions only occur during set game duration
     const uint8_t randomChance = random(0, 10);
-    const int randomStayTime = random(500, 800);
+    const int randomStayTime = random(1000, 2000);
     
     limitSwitch.loop();
     const uint8_t switchState = limitSwitch.getState();
@@ -57,20 +56,13 @@ void loop() {
       if (hitScorePrinted) {
         hitScorePrinted = false;
       }
-      if (randomChance < 2) {  // 20% chance of it going back down
-        servo.write(MOLE_DOWN);
-        previousMillis = currentMillis;
-      }
-      if (currentMillis - previousMillis >= moleStayTime) {  // Comes back up again after staying down for a random amount of time
-        servo.write(MOLE_UP);
-        moleStayTime = randomStayTime;  // Give it another random for next time
-      }
+      
+      
+
     } else if (limitSwitchState == DOWN) {  // Mole is Down (Hit)
       if (!moleHitDelay) {
         servo.write(MOLE_DOWN);  // Ensure we're not checking a servo that is currently in the hit delay (prevents false positives)
         hitTime = currentMillis;
-        score++;
-
         moleHitStartTime = currentMillis;
         moleHitDelay = true;
         if (limitSwitchState == DOWN && !hitScorePrinted) {
