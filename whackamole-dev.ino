69d205f4078dc2b135d642f6f240e4eddce6bc19
Yin <62259841+Yincard@users.noreply.github.com>
2024-04-11 23:33:45 -0400

    Update whackamole-dev.ino

--- whackamole-dev.ino
+++ whackamole-dev.ino
@@ -1,44 +1,45 @@
 #include <Servo.h>
 #include <ezButton.h>
 
-const int limitSwitchPin = 7;
-const int servoPin = 9;
-Servo servo;
-ezButton limitSwitch(limitSwitchPin);
+const int limitSwitchPins[] = {7};
+const int servoPins[] = {9};
+Servo servos[1];
+ezButton limitSwitches[1] = {ezButton(limitSwitchPins[0])};
 
-enum LimitSwitchState { UP,
-                        DOWN };
-enum ServoPosition { MOLE_UP = 180,
-                     MOLE_DOWN = 10 };
+enum LimitSwitchState {
+  UP,
+  DOWN
+};
 
-LimitSwitchState limitSwitchState = DOWN;
+LimitSwitchState limitSwitchStates[1] = {DOWN};
 
 bool startedPrinted = false;
 bool hitScorePrinted = false;
 bool endedPrinted = false;
+bool startUpDelay[1] = {true};
 
-constexpr unsigned long gameDuration = 60000;
-constexpr int HIT_DELAY = 1500;
-constexpr unsigned long moleStayTimeMin = 500; // control how long the mole stays down. (WHEN HIT)
-constexpr unsigned long moleStayTimeMax = 2000; // control how long the mole stays down.(WHEN HIT)
-constexpr unsigned long moleWaitTimeMin = 2000; // control the wait time when the mole is up and has not been hit.
-constexpr unsigned long moleWaitTimeMax = 3000; // control the wait time when the mole is up and has not been hit.
- 
-unsigned long previousMillis = 0;
-unsigned long hitTime = 0;
-unsigned long moleHitStartTime = 0;
-bool moleHitDelay = false;
-unsigned long moleStayTime = 800;
-unsigned long moleWaitTime = 2000;
-unsigned long moleUpStartTime = 0;
-bool moleUpDelay = false;
-
+constexpr unsigned long gameDuration = 45000;  
 unsigned long gameStartTime;
 
+struct Mole {
+  int MOLE_DOWN;
+  unsigned long moleHitStartTime;
+  bool moleHitDelay;
+  unsigned long hitTime;
+  unsigned long moleStayTime;
+  unsigned long HIT_DELAY;
+};
+
+Mole moles[1] = {{25, 0, false, 0, 800, 0}};
+
+const char* comboNames[1] = {"Harvard"};
+
 void setup() {
-  limitSwitch.setDebounceTime(50);
-  servo.write(MOLE_DOWN);
-  servo.attach(servoPin);
+  for (int i = 0; i < 1; i++) {
+    limitSwitches[i].setDebounceTime(50);
+    servos[i].write(moles[i].MOLE_DOWN);
+    servos[i].attach(servoPins[i]);
+  }
 
   Serial.begin(9600);
   gameStartTime = millis();
@@ -46,56 +47,51 @@ void setup() {
 
 void loop() {
   unsigned long currentMillis = millis();
+  
   if ((currentMillis - gameStartTime) <= gameDuration) {
-    limitSwitch.loop();
-    const uint8_t switchState = limitSwitch.getState();
-    limitSwitchState = (switchState == HIGH) ? UP : DOWN;
+    for (int i = 0; i < 1; i++) {
+      limitSwitches[i].loop();
+      const uint8_t switchState = limitSwitches[i].getState();
+      limitSwitchStates[i] = (switchState == HIGH) ? UP : DOWN;
 
-    if (!startedPrinted && !moleUpDelay) {
-      Serial.println("Start");
-      startedPrinted = true;
-      moleUpStartTime = currentMillis;
-      moleUpDelay = true;
-    }
+      if (!startedPrinted) {
+        Serial.println("Start");
+        startedPrinted = true;
+      }
 
-    if (moleUpDelay && (currentMillis - moleUpStartTime >= random(500, 2000))) {
-      servo.write(MOLE_UP);
-      moleWaitTime = random(moleWaitTimeMin, moleWaitTimeMax);
-      moleUpDelay = false;
-    }
-
-    if (!moleUpDelay) {
-      if (limitSwitchState == UP) {
-        if (currentMillis - moleHitStartTime >= moleStayTime) {
-          servo.write(MOLE_DOWN);
-          moleHitStartTime = currentMillis;
-          moleStayTime = random(moleStayTimeMin, moleStayTimeMax);
+      if (limitSwitchStates[i] == UP) {
+        if (startUpDelay[i] && (currentMillis - gameStartTime >= random(500, 2000))) {
+          servos[i].write(180);
+          startUpDelay[i] = false;
         }
-      } else if (limitSwitchState == DOWN) {
-        if (!moleHitDelay) {
-          hitTime = currentMillis;
-          moleHitStartTime = currentMillis;
-          moleHitDelay = true;
+
+        if (hitScorePrinted) {
+          hitScorePrinted = false;
+        }
+      } else if (limitSwitchStates[i] == DOWN) {
+        if (!moles[i].moleHitDelay) {
+          servos[i].write(moles[i].MOLE_DOWN);
+          moles[i].hitTime = currentMillis;
+          moles[i].moleHitStartTime = currentMillis;
+          moles[i].moleHitDelay = true;
           if (!hitScorePrinted) {
-            Serial.println("harvard");
+            Serial.println(comboNames[i]);
             hitScorePrinted = true;
           }
+          moles[i].HIT_DELAY = random(1000, 2000);
         }
       }
 
-      if (moleHitDelay && (currentMillis - moleHitStartTime >= HIT_DELAY)) {
-        servo.write(MOLE_UP);
-        moleHitDelay = false;
-        moleWaitTime = random(moleWaitTimeMin, moleWaitTimeMax);
+      if (moles[i].moleHitDelay && (currentMillis - moles[i].moleHitStartTime >= moles[i].HIT_DELAY)) {
+        servos[i].write(180);
+        moles[i].moleHitDelay = false;
       }
 
-      if (currentMillis - hitTime >= moleStayTime && limitSwitchState == DOWN) {
-        servo.write(MOLE_UP);
-        moleWaitTime = random(moleWaitTimeMin, moleWaitTimeMax);
+      if (moles[i].moleStayTime > 200) {
+        moles[i].moleStayTime -= 25;
       }
     }
-
-  } else if ((currentMillis - gameStartTime) >= gameDuration) {
+  } else {
     if (!endedPrinted) {
       Serial.println("End");
       endedPrinted = true;
@@ -103,6 +99,5 @@ void loop() {
     delay(1000);
     exit(0);
   }
-
   delay(100);
 }
